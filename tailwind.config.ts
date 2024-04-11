07d7dcf666b37d89aa8fc82d45d9c0018db5ea6b
Miro Storm <mirostorm@gmail.com>
2024-04-11 22:57:06 +0200

    fix: Fix font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     fontFamily: {
-      sans: ['"Roobert PRO"', 'Helvetica', 'Arial', 'sans-serif'],
-      body: ['"Roobert PRO"', 'Helvetica', 'Arial', 'sans-serif'],
-      title: ['"Roobert PRO"', 'Helvetica', 'Arial', 'sans-serif'],
+      sans: ['"Roobert"', 'Helvetica', 'Arial', 'sans-serif'],
+      body: ['"Roobert"', 'Helvetica', 'Arial', 'sans-serif'],
+      title: ['"Roobert"', 'Helvetica', 'Arial', 'sans-serif'],
     },
     extend: {
       colors: {
