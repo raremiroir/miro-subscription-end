7842738af2577778176f96f7a6bf866978243bfe
Miro Storm <mirostorm@gmail.com>
2024-04-29 00:27:34 +0200

    Remove footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,14 @@
-<script>
+<script lang="ts">
   import '../app.pcss';
   import Header from './Header.svelte';
 </script>
 
-<div class="flex flex-col min-h-screen bg-surface-50">
+<div class="flex flex-col h-screen max-h-screen bg-surface-50">
   <Header />
 
-  <main class="flex-1 flex flex-col p-4 w-full max-w-5xl mx-auto my-0 box-border">
-    <slot />
-  </main>
-
-  <footer class="flex flex-col justify-center items-center p-3 sm:px-0">
-    <p>visit <a href="https://kit.svelte.dev" class="font-bold">kit.svelte.dev</a> to learn SvelteKit</p>
-  </footer>
+  <div class="flex-1 flex items-center justify-center">
+    <main class="flex-1 flex flex-col p-4 w-full max-w-5xl mx-auto my-0 box-border">
+      <slot />
+    </main>
+  </div>
 </div>
